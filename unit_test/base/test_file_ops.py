1c98045813ccd5fc72eb075a5a18c48ac90ea407
guyy <guyy@dongyuansz.com>
2024-09-12 17:19:25 +0800

    US24091201 unit test

---
+++ unit_test/base/test_file_ops.py
@@ -0,0 +1,22 @@
+import mock
+import pytest
+
+from base.file_ops import FileOps
+from consts import error_code
+
+
+class TestFileOps(object):
+
+    @pytest.mark.parametrize("rmtree_ret, mkdir_ret, ret", [
+        (mock.Mock(), [mock.Mock(), mock.Mock(), mock.Mock()],
+         (error_code.OK, "finish creating empty okng dir")),
+        (mock.Mock(), [mock.Mock(), Exception("xxx"), mock.Mock()],
+         (error_code.INVALID_PATH, "failed to create [audio_data/], xxx"))
+    ])
+    @mock.patch("base.file_ops.os.mkdir")
+    @mock.patch("base.file_ops.shutil.rmtree")
+    def test_create_empty_okng(self, mock_rmtree, mock_mkdir, rmtree_ret, mkdir_ret, ret):
+        mock_rmtree.return_value = rmtree_ret
+        mock_mkdir.side_effect = mkdir_ret
+        result = FileOps().create_empty_okng("audio_data/")
+        assert result == ret
