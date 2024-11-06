1aed7882ab212eedfd2fbcc9c0c91181d05ac675
guyy <guyy@dongyuansz.com>
2024-11-06 10:22:11 +0800

    US24110401 Login window

---
+++ ui/stimulus_window.py
@@ -0,0 +1,14 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtWidgets import QLineEdit, QLabel, QMessageBox, QPushButton, QComboBox
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout
+
+
+class StimulusWindow(QDialog):
+
+    def __init__(self):
+        super().__init__()
+
+    def init_ui(self):
+        pass
