3c332db1044b045827822e2c064f077390068b84
guyy <guyy@dongyuansz.com>
2025-01-10 17:40:02 +0800

    US25011002 fix sound card latency problem

--- ui/sequence_widget.py
+++ ui/sequence_widget.py
@@ -491,13 +491,14 @@ class AnalyseWindow(QWidget):
         self.setStyleSheet(ui_style_const.qgroupbox_stytle +
                            "QDialog {border: 1px solid rgb(173, 173, 173);}")
         self.signal_analyse_dialog = SignalAnalysisWindow(self.signal_info)
-        self.signal_analyse_dialog.setMinimumSize(400, 390)
+        self.signal_analyse_dialog.setMinimumSize(330, 390)
         self.signal_analyse_dialog.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
         ai_analyse_dialog = QDialog()
         ai_analyse_layout = self.create_ai_analyse_layout()
         ai_analyse_dialog.setLayout(ai_analyse_layout)
         ai_analyse_dialog.setMaximumWidth(400)
+        ai_analyse_dialog.setMinimumSize(260, 390)
 
         btn_layout = QVBoxLayout()
         self.ok_btn.setIcon(QIcon(DEFAULT_DIR + "ui/ui_pic/sequence_pic/lvseyuan.png"))
@@ -532,9 +533,9 @@ class AnalyseWindow(QWidget):
         ai_title_layout.addItem(h_title_space)
 
         model_layout = QHBoxLayout()
-        model_label = QLabel(" 模型 ")
+        model_label = QLabel(" 模 型 ")
         model_label.setStyleSheet("background-color: #4472c4; color: white;")
-        model_label.setFixedHeight(25)
+        model_label.setFixedSize(65, 25)
         self.model_combo_box = QComboBox(self)
         self.model_combo_box.setFixedHeight(25)
         model_layout.addWidget(model_label)
