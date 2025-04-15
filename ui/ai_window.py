0377864edab15b317eee3df82206decc822cff6b
“DY-Qiang-Gao” <1241177060@qq.com>
2025-04-15 14:25:27 +0800

    GQ develop US2025041501 enhancement set new model sellect config file, only in test or train moidel print to process, one epich only print one line, modify ai_window

--- ui/ai_window.py
+++ ui/ai_window.py
@@ -167,22 +167,16 @@ class AiWindow(QDialog):
     def on_update_text(self, text, process_edit: QTextEdit):
         cursor = process_edit.model_structure_texteditor.textCursor()
         cursor.movePosition(QTextCursor.End)
-        enter_flag = False
         if text == "\n":
             cursor.insertText("\r")
         elif text.startswith("\n"):
-            if enter_flag:
-                cursor.insertText(text)
-                cursor.insertText("\r")
-            else:
-                content = text.lstrip('\n')
-                cursor.movePosition(QTextCursor.StartOfLine)
-                cursor.movePosition(QTextCursor.EndOfLine, QTextCursor.KeepAnchor)
-                cursor.removeSelectedText()
-                cursor.insertText(content)
+            content = text.lstrip('\n')
+            cursor.movePosition(QTextCursor.StartOfLine)
+            cursor.movePosition(QTextCursor.EndOfLine, QTextCursor.KeepAnchor)
+            cursor.removeSelectedText()
+            cursor.insertText(content)
         elif not "\r" in text:
             cursor.insertText(text)
-            enter_flag = True
         process_edit.model_structure_texteditor.setTextCursor(cursor)
         process_edit.model_structure_texteditor.ensureCursorVisible()
 
