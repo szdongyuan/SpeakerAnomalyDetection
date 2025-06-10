8ba5a0e76cca94706d8251336431b80909d3d441
“DY-Qiang-Gao” <1241177060@qq.com>
2025-06-10 09:50:09 +0800

    GQ develop BUG2025061001 update ai window set logmanager labely ai window

--- ui/ai_window.py
+++ ui/ai_window.py
@@ -208,7 +208,7 @@ class AiWindow(QDialog):
                                     config_path=result,
                                     mode=mode)
             process.current_thread = t
-            t.signalForText.connect(lambda text, process_edit = process: self.on_update_text(text, process_edit))
+            t.signalForText.connect(lambda text, process_edit=process: self.on_update_text(text, process_edit))
             sys.stdout = t
             print(action_str)
             t.start()
@@ -611,7 +611,7 @@ class TrainEvaluateThread(QThread):
 
 
 class AiBrainModelStructure(QDialog):
-    def __init__(self, parent=None, model_structure = None, model_name: str = None):
+    def __init__(self, parent=None, model_structure=None, model_name: str=None):
         super().__init__(parent)
         self.model_structure = model_structure
 
@@ -645,7 +645,7 @@ class AiBrainModelStructure(QDialog):
 
 
 class Process_Widget(QDialog):
-    def __init__(self, thread: TrainEvaluateThread = None):
+    def __init__(self, thread: TrainEvaluateThread=None):
         super().__init__()
 
         self.model_structure_texteditor = QTextEdit()
