484dc6f13c94168061e6a19b375ff9873d5b5050
wanglianzhi <2191695256@qq.com>
2025-07-14 10:02:44 +0800

    US250714001 Stimulus Signal Phase Repetition And Splash Screen Window, swept_sine_chirps.py, main_window_Launcher.py, splash_screen_window.py

---
+++ main_window_Launcher.py
@@ -0,0 +1,51 @@
+import sys
+
+from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from ui.splash_screen_window11 import Splash, LoaderThread
+
+
+class MainWindowLauncher(object):
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.splash = Splash()
+        self.splash.show()
+        self.app.processEvents()
+
+        self.loader = LoaderThread()
+        self.loader_thread = QThread()
+        self.loader.moveToThread(self.loader_thread)
+
+        self.loader.progress.connect(self.splash.update_progress)
+        self.loader.error.connect(self.on_error)
+        self.loader.finished.connect(self.on_loader_finished)
+
+        self.loader_thread.started.connect(self.loader.run)
+        self.loader.finished.connect(self.loader_thread.quit)
+        self.loader.finished.connect(self.loader.deleteLater)
+        self.loader.finished.connect(self.loader_thread.deleteLater)
+
+    def run(self):
+        self.loader_thread.start()
+        sys.exit(self.app.exec())
+
+    def on_loader_finished(self):
+        try:
+            self.splash.close()
+            from main_window import MainWindow
+            window = MainWindow()
+            window.show()
+        except Exception as e:
+            msg = f"主界面启动失败：{str(e)}"
+            QMessageBox.critical(None, "错误", msg)
+            sys.exit(1)
+
+    def on_error(self, msg):
+        self.splash.lab.setText(f"启动失败")
+        QMessageBox.critical(self.splash, "错误", msg)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    mwl = MainWindowLauncher()
+    mwl.run()
