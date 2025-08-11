595e570dfe07bea9581b146203e92e789257e467
“DY-Qiang-Gao” <1241177060@qq.com>
2025-08-11 11:14:50 +0800

    GQ develop US2025080801 enhancement split main is train evaluate and predict

--- base/evaluate_model.py
+++ base/evaluate_model.py
@@ -10,25 +10,18 @@ from base.model_config import init_model_from_config, preprocess_raw_signals
 from consts import error_code, model_consts
 
 
-def evaluate(predict_dir,
-             load_model_path=None,
-             model=None,
-             **kwargs):
+def evaluate(predict_dir, load_model_path=None, model=None, **kwargs):
     logger = LogManager.set_log_handler("evaluate")
 
     ret_code, ret = get_pre_labeled_audios(predict_dir)
     if ret_code != error_code.OK:
         logger.error("failed to load audio samples")
-        return json.dumps({"ret_code": ret_code,
-                           "ret_msg": ret,
-                           "result": ret})
+        return json.dumps({"ret_code": ret_code, "ret_msg": ret, "result": ret})
     signals, file_names, fs, labels = ret
 
     save_config_path = kwargs.get("config_path", model_consts.CONFIG_PATH)
     config_path = model_consts.DEFAULT_DIR + save_config_path
     preprocess_config = load_config(config_path=config_path, module_name="preprocess")
-    x_test = preprocess_raw_signals(signals, fs, preprocess_config)
-    y_test = labels
 
     if load_model_path:
         kwargs["config_path"] = config_path
@@ -36,10 +29,23 @@ def evaluate(predict_dir,
         model.load_model(load_model_path)
     if not model:
         logger.error("missing model")
-        return json.dumps({"ret_code": error_code.MISSING_MODEL,
-                           "ret_msg": "missing model",
-                           "result": "missing model"})
+        return json.dumps({"ret_code": error_code.MISSING_MODEL, "ret_msg": "missing model", "result": "missing model"})
 
+    acc_info, cm_info = evaluate_model(model, preprocess_config, signals, file_names, fs, labels, **kwargs)
+
+    model_detail = kwargs.get("model_detail", False)
+    if model_detail:
+        logger.info(model.model.summary())
+
+    ret_str = json.dumps({"ret_code": error_code.OK, "ret_msg": "finish evaluating", "result": [acc_info, cm_info]})
+    return ret_str
+
+
+def evaluate_model(model, preprocess_config, signals, file_names, fs, labels, **kwargs):
+    logger = LogManager.set_log_handler("evaluate")
+
+    x_test = preprocess_raw_signals(signals, fs, preprocess_config)
+    y_test = labels
     y_pred, pred_score = model.predict(x_test)
     len_test = len(y_test)
     acc = np.sum(y_pred == y_test) / len_test
@@ -66,11 +72,4 @@ def evaluate(predict_dir,
     if (verbose >> 4) % 2:
         dm.display_pred_score(file_names, labels, pred_score, to_csv=True)
 
-    model_detail = kwargs.get("model_detail", False)
-    if model_detail:
-        logger.info(model.model.summary())
-
-    ret_str = json.dumps({"ret_code": error_code.OK,
-                          "ret_msg": "finish evaluating",
-                          "result": [acc_info, cm_info]})
-    return ret_str
+    return acc_info, cm_info
