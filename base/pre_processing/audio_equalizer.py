d0964dfda5ff3528697552fbbcab5e5b103b9381
FYZAFH <fanh@dongyuansz.com>
2025-08-19 09:59:51 +0800

    Update audio_equalizer.py

--- base/pre_processing/audio_equalizer.py
+++ base/pre_processing/audio_equalizer.py
@@ -1,8 +1,7 @@
-import numpy as np
-from scipy.signal import windows
-
 from typing import Optional, Callable, Tuple
 
+import numpy as np
+from scipy.signal import windows
 
 class AudioEqualizer:
     """
