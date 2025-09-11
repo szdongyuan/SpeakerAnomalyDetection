9277102cd033d4d5efbbda7fdb4dd4a4014c39e2
“DY-Qiang-Gao” <1241177060@qq.com>
2025-09-11 10:06:07 +0800

    gq develop US2025091002 split analysis config window 1

--- consts/running_consts.py
+++ consts/running_consts.py
@@ -62,7 +62,7 @@ MODULES_LOAD = [
     ("加载 AI 模型训练模块", "ui.ai_window"),
     ("加载 校准模块", "ui.calibration_window"),
     ("加载 硬件选择模块", "ui.hardware_window"),
-    ("加载 分析流程模块", "ui.sequence_widget"),
+    ("加载 分析流程模块", "ui.sequence.sequence_widget"),
     ("加载 激励信号模块", "ui.stimulus_window"),
     ("加载 模型选择模块", "ui.operation_sequence"),
     ("加载 登录模块", "ui.login_window"),
