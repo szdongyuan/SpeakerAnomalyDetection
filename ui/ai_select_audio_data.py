b457bf357fb8bc4a9197268562957fe209ae3ef6
“DY-Qiang-Gao” <1241177060@qq.com>
2025-12-18 13:50:33 +0800

    BUG2025121801 ai window filter select audio, again enter this window check item error

--- ui/ai_select_audio_data.py
+++ ui/ai_select_audio_data.py
@@ -21,27 +21,46 @@ from ui.custom_ui_widget.audio_data_manage_dialog import AudioDataManageDialog
 
 
 class SelectAudioDataView(AudioDataManageDialog):
-    def __init__(self, logger: LogManager, select_row_list: list = list(), hide_select_not_label = True):
+    def __init__(self, logger: LogManager, select_data_list: list = list(), hide_select_not_label = True):
         super(SelectAudioDataView, self).__init__(logger, hide_select_not_label)
 
         self.logger = logger
         self.clicked_ok_flag = False
-        self.init_select_row_list = select_row_list
+        self.init_select_data_list = select_data_list
         self.select_row_list = list()
         self.select_ng_num = int()
         self.select_ok_num = int()
+
+        self.filter_signal.connect(self.refresh_data)
 
         self.init_ui()
 
     def init_ui(self):
 
         self.set_bottom_layout()
-        self.set_checkacle_of_row(row_num=self.init_select_row_list)
+        self.get_select_row_by_init_list()
+        self.set_checkacle_of_row(row_num=self.select_row_list)
 
     def set_checkacle_of_row(self, row_num: list[int]):
         for row in row_num:
             item = self.model().item(row, 0)
             item.setCheckState(Qt.Checked)
+
+    def get_select_row_by_init_list(self):
+        for key, value in self.init_select_data_list.items():
+            for row, item in enumerate(self.all_audio_data):
+                # 从文件路径中提取文件名
+                file_path = item[1]
+                # 比较文件名与key是否相同
+                if file_path == key:
+                    self.select_row_list.append(row)
+                    break
+
+    def refresh_data(self):
+        """刷新数据：在筛选后重新设置选中状态"""
+        self.select_row_list.clear()
+        self.select_ok_num = 0
+        self.select_ng_num = 0
 
     def load_audio_data_to_view(self):
         self.all_audio_data = [i for i in self.all_audio_data if any(v in i for v in ["OK", "NG"])]
@@ -104,7 +123,7 @@ class SelectAudioDataView(AudioDataManageDialog):
         if self.clicked_ok_flag:
             for i in self.select_wave_data.values():
                 data[i[1]] = [i[3], i[5]]
-            return (data, (self.select_ok_num, self.select_ng_num), self.select_row_list)
+            return (data, (self.select_ok_num, self.select_ng_num))
         return None
 
 
